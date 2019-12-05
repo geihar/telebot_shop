833535c5bc070a67651a06c36795cc339397c685
geihar <40303929+geihar@users.noreply.github.com>
2019-12-05 18:17:17 +0200

    Update config.py

--- Project_telebot/config.py
+++ Project_telebot/config.py
@@ -1,5 +1,4 @@
-# TOKEN = "877628109:AAHixswW9_CdVkDRyUgCgp9cDjs0aiwGpco"
-TOKEN = "1054537528:AAFFEK-eR6x_x05ZwaW7PVMFIVQBr069AI0"
+TOKEN = "877628109:AAHixswW9_CdVkDRyUgCgp9cDjs0aiwGpco"
 
 host = '127.0.0.1'
 webhook_url = f'https://34.68.0.67'#set webhook for this url
